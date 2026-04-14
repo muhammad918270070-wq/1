02ae7bf7d6e5ff1e7c742cf7184d2bd259d1cd08
muhammad918270070-wq <muhammad918270070@gmail.com>
2026-04-14 19:18:38 +0500

    Refactor app title and add data loading function
    Updated title and added a description for the app. Implemented data loading function using California housing dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
 
-st.title('asasasasasasasas')
+st.title('My first project')
+st.markdown("### Smart real estate valuation powered by Machine Learning")
 
-st.markdown('### aeiudhfgasdfbgljasdfg')
+@st.cache_data
+def load_data():
+    housing = fetch_california_housing()
+    X = pd.DataFrame(housing.data, columns=housing.feature_names)
+    y = pd.Series(housing.target, name="PRICE")
+    return X, y
+
+X, y = load_data()
